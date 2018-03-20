e33d04bae5106cfe0f4f6e0c6d8e618127664d74
Cameron <thcameron@w15166.davidson.edu>
2018-03-19 21:28:28 -0400

    update

--- tests/install_tests.sh
+++ tests/install_tests.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+gfortran -O3 -cpp test_eiscor.f90 -o test_eiscor -I /Users/thcameron/Documents/eiscor/include /Users/thcameron/eiscor/lib/libeiscor.dylib.0.2.0
+
+
 # compile other_src (except for poly_zeros which is included as module)
-gfortran -c -O2 other_src/*.f90 -lblas
-gfortran -c -O2 other_src/turnovers/*.f90 -lblas
-rm poly_zeroes.mod
-rm pzeros.o
+#gfortran -c -O2 other_src/*.f90 -lblas
+#gfortran -c -O2 other_src/turnovers/*.f90 -lblas
+#rm poly_zeroes.mod
+#rm pzeros.o
 
 # create library
-ar crv libfpml_test.a *.o
-cp libfpml_test.a lib/
+#ar crv libfpml_test.a *.o
+#cp libfpml_test.a lib/
 
 # compile rand_poly.f90
-gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 rand_poly.f90 -L lib -lfpml_test -lblas -o rand_poly
+#gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 rand_poly.f90 -L lib -lfpml_test -lblas -o rand_poly
 # compile spec_poly.f90
-gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 spec_poly.f90 -L lib -lfpml_test -lblas -o spec_poly
+#gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 spec_poly.f90 -L lib -lfpml_test -lblas -o spec_poly
 # compile unity.f90
-gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 unity.f90 -L lib -lfpml_test -lblas -o unity
+#gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 unity.f90 -L lib -lfpml_test -lblas -o unity
 # compile init_est.f90
-gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 init_est.f90 -o init_est
+#gfortran -O2 other_src/pzeros.f90 ../src/fpml.f90 init_est.f90 -o init_est
 # compile methods.f90
-gfortran -O2 methods.f90 -o methods
+#gfortran -O2 methods.f90 -o methods
 # compile conv.f90
-gfortran -O2 ../src/fpml.f90 conv.f90 -o conv
+#gfortran -O2 ../src/fpml.f90 conv.f90 -o conv
 
 # clean up
 rm *.mod
